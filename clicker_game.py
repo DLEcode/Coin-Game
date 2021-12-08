ae7c19a23c2dc9a7f49a8c682eabd8f215a66065
DLEcode <95715845+DLEcode@users.noreply.github.com>
2021-12-08 21:58:17 +0000

    Add files via upload

--- clicker_game.py
+++ clicker_game.py
@@ -1,42 +1,160 @@
 from clicker import Pet
-import time as t
-print("Hello, in this game, you get coins every second, and to get more coins, you need to buy more pets or multipliers. The pets are able to multiply your coins/second and the higher the multiplier of the pet, the more expensive it costs. The multipliers, multiply your total money outcome every second by a fixed amount, they cost money.")
-print("To get to the shop, type 'shop'; to find out your earnings, type 'coins'; to buy pets, type 'buy pet', and then type the number when asked to.")
+#import time as t
+#from Python import pynput
+#import Key, Listener
+print("Hello, in this game, you get coins every time you type.Your aim is to get the most coins, and you need to buy more pets or multipliers in order to do so. The pets are able to give you more coins: the higher the multiplier of the pet, the more coins you get, and the more expensive it costs. The multipliers multiply your total money outcome every second by a fixed amount, they cost money.")
+print("To get to the shop, press 's'; to find out your coins, press 'c'; to see your pets, press 'p'; to see activated/equipt pets, press 'a'; to see income, press 'i'; to buy pets, press 'b', and then type the name when asked to do so.")
 coins = 0
-Wasp = Pet("Wasp", 2.5)
-Bunny = Pet("Rabbit", 5)
-Rabbit = Pet("Rabbit", 7.5)
-Mole = Pet("Rabbit", 10)
-Fox = Pet("Fox", 12.5)
-Penguin = Pet("Penguin", 15)
-Cat = Pet("Cat", 17.5)
-Dog = Pet("Dog", 20)
-Fast_Bunny = Pet ("Fast Bunny", 50)
-Fast_Rabbit = Pet("Fast Rabbit", 75)
-Fast_Mole = Pet("Fast Mole", 100)
-Fast_Fox = Pet("Fast Fox", 125)
-Fast_Penguin = Pet("Fast Penguin", 150)
-Fast_Cat = Pet("Fast Cat", 175)
-Fast_Dog = Pet("Fast Dog", 200)
-Elephant = Pet("Elephant", 50)
-Leopard = Pet("Leopard", 60)
-Tiger = Pet("Tiger", 70)
-Alligator = Pet("Alligator", 80)
-Scorpion = Pet("Scorpion", 90)
-Rhinoceros = Pet("Rhinoceros", 100)
-Wasps = Pet("Swarm of Wasps", 2500)
-Elephants = Pet("Herd of Elephants", 50000)
-Leopards = Pet("Leap of Leopards", 60000)
-Tigers = Pet("Streak of Tigers", 70000)
-Alligators = Pet("Congregation of Alligator", 80000)
-Scorpions = Pet("Bed of Scorpions", 90000)
-Rhinoceroses = Pet("Crash of Rhinoceroses", 100000)
-pets_list = (Wasp, Bunny, Rabbit, Mole, Fox, Penguin, Cat, Dog, Fast_Bunny, Fast_Rabbit, Fast_Mole, Fast_Fox, Fast_Dog, Elephant, Leopard, Tiger, Alligator, Scorpion, Rhinoceros, Wasps, Elephants, Leopards, Tigers, Alligators, Scorpions, Rhinoceroses)
-#pets_dictionary = (1:Wasp, 2:Bunny, 3:Rabbit, 4:Mole, 5:Fox, 6:Penguin, 7:Cat, 8:Dog, 9:Fast_Bunny, 10:Fast_Rabbit, 11:Fast_Mole, 12:Fast_Fox, 13:Fast_Dog, 14:Elephant, 15:Leopard, 16:Tiger, 17:Alligator, 18:Scorpion, 19:Rhinoceros, 20:Wasps, 21:Elephants, 22:Leopards, 23:Tigers, 24:Alligators, 25:Scorpions, 26:Rhinoceroses)
+Wasp = Pet("Wasp", 2.5, 10)
+Bunny = Pet("Bunny", 5, 60)
+Rabbit = Pet("Rabbit", 7.5, 100)
+Mole = Pet("Mole", 10, 1000)
+Fox = Pet("Fox", 12.5, 1500)
+Penguin = Pet("Penguin", 15, 2000)
+Cat = Pet("Cat", 17.5, 3000)
+Dog = Pet("Dog", 20, 5000)
+Fast_Bunny = Pet ("Fast Bunny", 50, 6000)
+Fast_Rabbit = Pet("Fast Rabbit", 75, 20000)
+Fast_Mole = Pet("Fast Mole", 100, 100000)
+Fast_Fox = Pet("Fast Fox", 125, 150000)
+Fast_Penguin = Pet("Fast Penguin", 150, 300000)
+Fast_Cat = Pet("Fast Cat", 175, 500000)
+Fast_Dog = Pet("Fast Dog", 200, 1000000)
+Elephant = Pet("Elephant", 50, 6000)
+Leopard = Pet("Leopard", 60, 8000)
+Tiger = Pet("Tiger", 70, 15000)
+Alligator = Pet("Alligator", 80, 60000)
+Scorpion = Pet("Scorpion", 90, 80000)
+Rhinoceros = Pet("Rhinoceros", 100, 100000)
+Wasps = Pet("Swarm of Wasps", 2500, 100000000)
+Elephants = Pet("Herd of Elephants", 50000, 2000000000)
+Leopards = Pet("Leap of Leopards", 60000, 300000000000)
+Tigers = Pet("Streak of Tigers", 70000, 40000000000000)
+Alligators = Pet("Congregation of Alligator", 80000, 8000000000000000)
+Scorpions = Pet("Bed of Scorpions", 90000, 30000000000000000000)
+Rhinoceroses = Pet("Crash of Rhinoceroses", 100000, 1000000000000000000000000)
+pets_list = [Wasp, Bunny, Rabbit, Mole, Fox, Penguin, Cat, Dog, Fast_Bunny, Fast_Rabbit, Fast_Mole, Fast_Fox, Fast_Dog, Elephant, Leopard, Tiger, Alligator, Scorpion, Rhinoceros, Wasps, Elephants, Leopards, Tigers, Alligators, Scorpions, Rhinoceroses]
+My_pets = []
+
+
+"""
+def key_press():
+    key = input("")
+    if key == 's':
+        for item in pets_list:
+            print(item.name)
+            print(item.multiplier)
+            print(item.cost)
+    elif key == 'c':
+        print(coins)
+    elif key == 'b':
+        answer = input("Please type the name of the pet you would like to buy")
+        if coins > answer.cost:
+            answer.buy
+            coins = coins - answer.cost
+            My_pets.insert(0, answer)
+    elif key == 'e':
+        answer = input("Please type the name of the pet you would like to equipt")
+        if answer.active == True:
+            for item in My_pets:
+                item.active = False
+            answer.active = True
+        else:
+            print("Pet is not owned or does not exist.")
+    else:
+        print("Please type in something suitable")
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        return False
+
+with keyboard.Listener(
+        key_press = key_press,
+        on_release=on_release) as listener:
+    listener.join()
+"""
+#experimenting with pynput, could not manage to get it to work
+
 while True:
-    t.sleep(1)
+#    t.sleep(1)
+    coins = coins + 1
     for pet in pets_list:
         if pet.active == True:
-            coins = coins + 1 * pet.multiplier
+            coins = coins + pet.multiplier
         else:
-            coin = coins + 1
+            continue
+    key = input("")
+    if key == 's':
+        for item in pets_list:
+            print("Name: {0} , Multiplier: {1} , Cost: {2}".format(item.name, item.multiplier, item.cost))
+#            print(item.multiplier)
+#            print(item.cost)
+    elif key == 'c':
+        print(coins)
+    elif key == 'b':
+        answer = input("Please type the name of the pet you would like to buy: ")
+        for item in pets_list:
+            if item.name == answer:
+                if coins > item.cost:
+                    item.buy
+                    coins = coins - item.cost
+                    My_pets.append(item)
+                    print("Pet bought!")
+                else:
+                    continue
+            else:
+                continue
+#        if coins > answer.cost:
+#           answer.buy
+#            coins = coins - answer.cost
+#            My_pets.insert(0, answer)
+    elif key == 'e':
+        answer = input("Please type the name of the pet you would like to activate/equipt: ")
+        for item in My_pets:
+            if item.name == answer:    #finding if the pet is owned
+#                for item in My_pets:   #unactivating other pets in use... 
+#                    item.acive = False       #I decided to make it so that pet stats can 'stack': they add to the amount of coins per second
+                for item in My_pets:
+                    if item.name == answer: #finding the pet that is owned (again)
+                            item.active = True
+                            print("All of that type of pet have been equipt!")
+                    else:
+                        continue
+            else:
+                print("Finding pet...")
+    elif key == 'p':
+#        print(My_pets)
+        for item in My_pets:
+            print(item.name)
+#        if answer.active == True:
+#            for item in My_pets:
+#                item.active = False
+#            answer.active = True
+#        else:
+#            print("Pet is not owned or does not exist.")
+    elif key == 'a':
+        for item in My_pets:
+            if item.active == True:
+                print("{} is activated".format(item.name))
+            else:
+                print("{} not activated...".format(item.name))
+        if len(My_pets) == 0:
+            print("No pets activated/equipt.")
+    elif key == 'i':
+        x = 0
+        for item in My_pets:
+            if item.active == True:
+                x = x + item.multiplier + 1
+            else:
+                print("A pet is not active!")
+        print("Current income: {}".format(x))
+   # elif key == 'f':
+   #     coins = 10000000000000000000000000000000000
+    else:
+        print("Please type in something suitable")
+
+
+
+
+
+            
