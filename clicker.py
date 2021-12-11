e6cdbec146b8f169f110bcd60b9c5574260bba48
DLEcode <95715845+DLEcode@users.noreply.github.com>
2021-12-11 12:34:05 +0000

    Add files via upload

--- clicker.py
+++ clicker.py
@@ -1,11 +1,12 @@
 class Pet:
 
-    def __init__(self, name, multiplier):
+    def __init__(self, name, multiplier, cost):
         self.name = name
         self.active = False
         self.owned = False
         self.multiplier = multiplier
-            
+        self.cost = cost
+"""    
     def buy(self):
         self.owned = True
         
@@ -14,5 +15,236 @@ class Pet:
             self.active = True
         else:
             print("This pet is not owned.")
-            
-            
+
+"""          
+class Wasp:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 2.5
+        self.active = False
+        self.owned = False
+        self.cost = 10
+        
+class Bunny:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 5
+        self.active = False
+        self.owned = False
+        self.cost = 60
+
+class Rabbit:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 7.5
+        self.active = False
+        self.owned = False
+        self.cost = 100
+        
+class Mole:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 10
+        self.active = False
+        self.owned = False
+        self.cost = 1000
+        
+class Fox:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 12.5
+        self.active = False
+        self.owned = False
+        self.cost = 1500
+        
+class Penguin:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 15
+        self.active = False
+        self.owned = False
+        self.cost = 2000
+
+class Cat:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 17.5
+        self.active = False
+        self.owned = False
+        self.cost = 3000
+
+class Dog:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 20
+        self.active = False
+        self.owned = False
+        self.cost = 5000
+        
+class Fast_Bunny:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 50
+        self.active = False
+        self.owned = False
+        self.cost = 6000
+        
+class Fast_Rabbit:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 75
+        self.active = False
+        self.owned = False
+        self.cost = 20000
+        
+class Fast_Mole:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 100
+        self.active = False
+        self.owned = False
+        self.cost = 100000
+
+class Fast_Fox:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 125
+        self.active = False
+        self.owned = False
+        self.cost = 150000
+
+class Fast_Penguin:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 150
+        self.active = False
+        self.owned = False
+        self.cost = 300000
+        
+class Fast_Cat:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 175
+        self.active = False
+        self.owned = False
+        self.cost = 500000
+        
+class Fast_Dog:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 200
+        self.active = False
+        self.owned = False
+        self.cost = 1000000
+        
+class Elephant:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 50
+        self.active = False
+        self.owned = False
+        self.cost = 6000
+        
+class Leopard:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 60
+        self.active = False
+        self.owned = False
+        self.cost = 8000
+
+class Tiger:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 70
+        self.active = False
+        self.owned = False
+        self.cost = 15000
+
+class Alligator:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 80
+        self.active = False
+        self.owned = False
+        self.cost = 60000
+
+class Scorpion:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 90
+        self.active = False
+        self.owned = False
+        self.cost = 80000
+
+class Rhinoceros:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 100
+        self.active = False
+        self.owned = False
+        self.cost = 100000
+
+class Wasps:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 25000
+        self.active = False
+        self.owned = False
+        self.cost = 10000000
+
+class Elephants:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 500000
+        self.active = False
+        self.owned = False
+        self.cost = 20000000
+
+class Leopards:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 6000000
+        self.active = False
+        self.owned = False
+        self.cost = 300000000
+
+class Tigers:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 70000000
+        self.active = False
+        self.owned = False
+        self.cost = 4000000000
+
+class Alligators:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 800000000
+        self.active = False
+        self.owned = False
+        self.cost = 80000000000
+
+class Scorpions:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 90000000000
+        self.active = False
+        self.owned = False
+        self.cost = 3000000000000
+        
+class Rhinoceroses:
+    def __init__(self, name):
+        self.name = name
+        self.multiplier = 10000000000000
+        self.active = False
+        self.owned = False
+        self.cost = 100000000000000000
+
+
+
+
+
+
+
+        
